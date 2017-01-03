8bac5a3c500bda7429c5e84d7b2745a0e58bc026
Manish Sharma <manisherma81@gmail.com>
2017-01-04 02:26:27 +0530

    Postfix and prefix values
    Postfix and prefix values of number 5

---
+++ Numbers.java
@@ -0,0 +1,11 @@
+class Numbers
+ {
+    public static void main(String arg[])
+    {
+      int i=5;
+      int k=5;
+      System.out.println("The values of 5 after prefix increment"+ ++i);
+      System.out.println("The values of 5 after postfix increment"+ k++);
+      }
+} 
+     
