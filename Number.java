7c60e0a94b8a486cf589278e2fdf13ae3baf10e3
Manish Sharma <manisherma81@gmail.com>
2017-01-04 02:35:51 +0530

    Number is divisible by 2nd no or not

---
+++ Number.java
@@ -0,0 +1,18 @@
+class Number
+{
+   public static void main(String arg[])
+   {
+     int num1=10;
+     int num2=2;
+     if(num1%num2==0)
+     {
+       System.out.println("The num1 is divisible by num2");
+     }
+     else
+     {
+       System.out.println("The num1 is not divisible by num2");
+     }
+}
+}
+     
+       
