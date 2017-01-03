6d4fff306d2dec5d84582def78b167f2fd7ad785
Manish Sharma <manisherma81@gmail.com>
2017-01-04 02:43:15 +0530

    Display Employee details
    using toString()

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -1,44 +1,24 @@
-import java.util.*;
+
 class EmployeeDetails
-    {  
-      public void showMenu()
-      { 
-      int option;
-      Scanner s = new Scanner(System.in);
-      System.out.println("-------MENU-------");
-      System.out.println("1. Enter Data");
-      System.out.println("2. Display Data");
-      System.out.println("3. Exit");
-      System.out.println("Choose the option");
-      option = s.nextInt();
-      switch(option)
-      {
-       case 1:
-          EmployeeDetails obj = new EmployeeDetails();
-          obj.enterData();
-          break;
-      case 2:
-          EmployeeDetails obj1 = new EmployeeDetails();
-          obj1.displayData();
-          break;
-      case 3:
-          break;
-      default:
-          System.out.println("Invalid option");
-          break;
-     }
-}
-      public void enterData()
-      {
-        System.out.println("enterData method is invoked...");
-       }
-      public void displayData()
-      {
-       System.out.println("displayData method is invoked...");
-      }
-public static void main(String arg[])
 {
-   EmployeeDetails obj2 = new EmployeeDetails();
-   obj2.showMenu();
-}
-}
+   String empID,name,designation,department;
+   public EmployeeDetails()
+   {
+     empID = "S151160800159";
+     name = "Dheeraj Sharma";
+     designation = "Software Developer";
+     department = "IT";
+   }
+   public String toString()
+   {
+     return "Employee ID: " + empID + 
+     " Employee Name: " + name + 
+     " Designation: " + designation + 
+     " Department :" + department;
+   }
+   public static void main(String arg[])
+   {
+    EmployeeDetails obj = new EmployeeDetails();
+    System.out.println(obj);
+   }
+ }     
