1f7c07bc0e950c6074754d6629da270178da3007
Manish Sharma <manisherma81@gmail.com>
2017-01-04 02:31:32 +0530

    Display Numeric  pattern

---
+++ NumericPattern.java
@@ -0,0 +1,14 @@
+class NumericPattern
+{
+  public static void main(String arg[])
+  {
+    for(int i=5;i>0;i--)
+    { 
+     for(int j=1;j<=i;j++)
+     {
+     System.out.println(j);
+     }
+     System.out.println();
+    }
+}
+}
